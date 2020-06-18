8a6ba730bb8f23ea1a6860a1a2e32439bb093054
arixmf <arik.lekar@microfocus.com>
2020-06-18 10:04:42 -0400

    working versin

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
-FROM alpine:3.12.0
+FROM ubuntu:20.04
 
 ARG CURATOR_VERSION
 
-RUN apk --update add python py-setuptools py-pip && \
-    pip install elasticsearch-curator==${CURATOR_VERSION} && \
-    apk del py-pip && \
-    rm -rf /var/cache/apk/*
+RUN apt-get update && \
+	apt-get install -y python3 && \
+	apt-get install -y python3-pip && \
+	apt-get install -y curl && \
+	apt-get -y install cron
+
+RUN pip3 install elasticsearch-curator==${CURATOR_VERSION}
 
 COPY entrypoint.sh /usr/bin/entrypoint.sh
 RUN chmod +x /usr/bin/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-mkdir -p /etc/periodic/${PERIOD}
-cat > /etc/periodic/${PERIOD}/0-curator <<EOF
+cat > /usr/bin/run_curator.sh <<EOF
 #!/bin/sh
-/usr/bin/curator_cli $@ \
-    delete_indices \
-    --ignore_empty_list \
-    --filter_list '[{"filtertype":"age","source":"name","direction":"older","unit":"days","unit_count":${KEEP_DAYS},"timestring": "%Y.%m.%d"},{"filtertype":"pattern","kind":"prefix","value":"${INDEX_PREFIX}"}]'
+
+/usr/local/bin/curator_cli --host ${ELASTIC_HOST} --port ${ELASTIC_PORT} delete_indices --ignore_empty_list --filter_list '[{"filtertype": "age", "source": "name", "direction": "older", "unit": "${RETENTION_UNIT_TYPE}", "unit_count": ${RETENTION_UNITS}, "timestring": "${RETENTION_TIME_STRING}"}, {"filtertype": "pattern", "kind": "regex", "value": "${RETENTION_INDEX_PATTERN}"}]'
 EOF
-chmod +x /etc/periodic/${PERIOD}/0-curator
-echo "cron: /etc/periodic/${PERIOD}/0-curator"
-echo "command: $(cat /etc/periodic/${PERIOD}/0-curator)"
-exec crond -f
+
+chmod +x /usr/bin/run_curator.sh
+
+cat > /etc/cron.d/curator_cron <<EOF
+${CRON_PERIOD} /usr/bin/run_curator.sh >> /var/log/cron.log 2>&1
+EOF
+
+chmod 0644 /etc/cron.d/curator_cron
+crontab /etc/cron.d/curator_cron
+
+touch /var/log/cron.log
+
+cron && tail -f /var/log/cron.log
